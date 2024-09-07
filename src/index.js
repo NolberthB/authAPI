ed07087c83858f32239d66ff3f3d8062fc06de00
= <=>
2024-09-06 18:11:28 -0600

    feat: agrega manejo de errores al iniciar el servidor

--- src/index.js
+++ src/index.js
@@ -3,25 +3,40 @@ import 'dotenv/config'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 
+import errorHandler from './middlewares/errorHandler.js'
 import { PORT } from './configs/env.js'
-import connectDB from './configs/mongoDB.js'
+import connectDB from './db/mongoDB.js'
 import authRouters from './routers/authRouters.js'
 
 const app = express()
 
-connectDB() // <- Mongo connection
+const startServer = async () => {
+  try {
+    await connectDB() // <- connection database
 
-// middlewares
-app.use(express.json())
-app.use(cookieParser())
+    // middlewares
+    app.use(express.json())
+    app.use(cookieParser())
 
-// routers
-app.use('/auth', authRouters)
+    // routers
+    app.use('/auth', authRouters)
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Hello API Auth Service'
-  })
-})
+    app.get('/', (req, res) => {
+      res.send({
+        message: 'Hello API Auth Service'
+      })
+    })
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    // middleware catcher errors
+    app.use(errorHandler)
+
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    //
+  } catch (error) {
+    // Loguear el error y finalizar el proceso si el servidor no pudo iniciar
+    console.error('Error starting server:', error.message)
+    process.exit(1) // Termina el proceso con un error
+  }
+}
+
+startServer()
